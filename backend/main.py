c4fb57cc392f7fea8db71314f90982910b4165de
pulkitchhabra33-hue <pulkitchhabra33@gmail.com>
2026-05-15 17:56:13 +0530

    Connecting Backend and Frontent

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routes.generate import router as generate_router
 
-app= FastAPI()
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(generate_router)
 
 @app.get("/")

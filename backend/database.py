46e6e975c4a2e2c97a8df3c97268668469d481d0
pulkitchhabra33-hue <pulkitchhabra33@gmail.com>
2026-05-27 22:58:33 +0530

    Fixing issue in database

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL= "sqlite:///./test.db"
 
-engine= create_engine(DATABASE_URL, connect_args= ("check_same_thread", False))
+engine= create_engine(DATABASE_URL, connect_args= {"check_same_thread": False})
 
 SessionLocal= sessionmaker(autocommit= False, autoflush= False, bind= engine)
 

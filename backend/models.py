e848d932ccebfc6bdc9d05fa6bf9a666b51889de
pulkitchhabra33-hue <pulkitchhabra33@gmail.com>
2026-05-28 19:13:33 +0530

    saving main file

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ class User(Base):
     __tablename__= "users"
 
     id= Column(Integer, primary_key= True, index= True)
-    email= Column(String, unique= True, index= True)
-    password= Column(String)
+    email= Column(String(255), unique= True, index= True)
+    password= Column(String(255))
 
     free_generations_used= Column(Integer, default= 0)

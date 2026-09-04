441f4736ad7e38b4ab996b51e1b3d8372b5b4472
pulkitchhabra33-hue <pulkitchhabra33@gmail.com>
2026-09-04 17:21:39 +0530

    Restoring ACCESS_TOKEN_EXPIRE_MINUTES

--- backend/auth.py
+++ backend/auth.py
@@ -32,7 +32,7 @@ secret_key= os.getenv("SECRET_KEY")
 oauth2_scheme= OAuth2PasswordBearer(tokenUrl= "login")
 
 algorithm= "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES= 1
+ACCESS_TOKEN_EXPIRE_MINUTES= 60
 
 pwd_context= CryptContext(schemes= ["pbkdf2_sha256"], deprecated= "auto")
 

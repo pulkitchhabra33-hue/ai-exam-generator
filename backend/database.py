a22f5bc518dd4339c87c8c82d2761404e0d8ac9d
pulkitchhabra33-hue <pulkitchhabra33@gmail.com>
2026-09-21 15:45:08 +0530

    Pushing db file

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,38 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL= "sqlite:///./test.db"
 
-engine= create_engine(DATABASE_URL, connect_args= {"check_same_thread": False})
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./test.db"
+)
 
-SessionLocal= sessionmaker(autocommit= False, autoflush= False, bind= engine)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
 
-Base= declarative_base()
+connect_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {
+        "check_same_thread": False
+    }
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args=connect_args
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
